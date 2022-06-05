51e161731d7708f8351df81c1f19817d028cbebc
masikotimo <masikotimo@gmail.com>
2022-06-05 19:00:33 +0300

    Initial commit of the react app

---
+++ src/api/testQuestions.js
@@ -0,0 +1,65 @@
+var testQuestions = [
+  {
+    question:
+      "You’re really busy at work and a colleague is telling you their life story and personal woes. You:",
+    answers: [
+      {
+        type: "Introvert",
+        content: "Don’t dare to interrupt them",
+      },
+
+      {
+        type: "Extrovert",
+        content: "Listen, but with only with half an ear",
+      },
+    ],
+  },
+  {
+    question:
+      "You’ve been sitting in the doctor’s waiting room for more than 25 minutes. You:",
+    answers: [
+      {
+        type: "Introvert",
+        content: "Look at your watch every two minutes",
+      },
+
+      {
+        type: "Extrovert",
+        content:
+          "Explain to other equally impatient people in the room that the doctor is always running late",
+      },
+    ],
+  },
+  {
+    question:
+      "You’re having an animated discussion with a colleague regarding a project that you’re in charge of. You:",
+    answers: [
+      {
+        type: "Introvert",
+        content: "Don’t dare contradict them",
+      },
+
+      {
+        type: "Extrovert",
+        content: "Defend your own point of view, tooth and nail",
+      },
+    ],
+  },
+  {
+    question: "You are taking part in a guided tour of a museum. You: ",
+    answers: [
+      {
+        type: "Introvert",
+        content:
+          "Are a bit too far towards the back so don’t really hear what the guide is saying",
+      },
+
+      {
+        type: "Extrovert",
+        content: "Make sure that everyone is able to hear properly",
+      },
+    ],
+  },
+];
+
+export default testQuestions;
